1f71962d7e9667f334a9b1d81b834405492b6aaa
coproductions <constantin.dumba@gmail.com>
2016-03-01 15:08:01 -0800

    removed sys

--- public/d3logic.js
+++ public/d3logic.js
@@ -80,64 +80,64 @@ setInterval(function(){
 
 
 
-var w = 400;
-var h = 400;
-var r = h/2;
-var color = d3.scale.category20c();
+// var w = 400;
+// var h = 400;
+// var r = h/2;
+// var color = d3.scale.category20c();
 
 
 
-var vis = d3.select('#chart').append("svg:svg").data([memValues]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
-var pie = d3.layout.pie().value(function(d){return d.value;});
+// var vis = d3.select('#chart').append("svg:svg").data([memValues]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+// var pie = d3.layout.pie().value(function(d){return d.value;});
 
-// declare an arc generator function
-var arc = d3.svg.arc().outerRadius(r);
+// // declare an arc generator function
+// var arc = d3.svg.arc().outerRadius(r);
 
-// select paths, use arc generator to draw
-var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+// // select paths, use arc generator to draw
+// var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
 
 
-arcs.append("svg:path")
-    .attr("fill", function(d, i){
-        return color(i);
-    })
-    .attr("d", function (d) {
-        // log the result of the arc generator to show how cool it is :)
-        console.log(arc(d));
-        return arc(d);
-    });
+// arcs.append("svg:path")
+//     .attr("fill", function(d, i){
+//         return color(i);
+//     })
+//     .attr("d", function (d) {
+//         // log the result of the arc generator to show how cool it is :)
+//         console.log(arc(d));
+//         return arc(d);
+//     });
 
-// add the text
-arcs.append("svg:text").attr("transform", function(d){
-      d.innerRadius = 0;
-      d.outerRadius = r;
-    return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
-    return memValues[i].label;}
-    );
+// // add the text
+// arcs.append("svg:text").attr("transform", function(d){
+//       d.innerRadius = 0;
+//       d.outerRadius = r;
+//     return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+//     return memValues[i].label;}
+//     );
 
-setInterval(function(){
-vis = d3.select('#chart').data([memValues]);
-pie = d3.layout.pie().value(function(d){return d.value;});
-arcs = vis.selectAll("g.slice").data(pie).select('svg:g')
-arcs.select('svg:path').attr("fill", function(d, i){
-        return color(i);
-    })
-    .attr("d", function (d) {
-        // log the result of the arc generator to show how cool it is :)
-        console.log(arc(d));
-        return arc(d);
-    });
+// setInterval(function(){
+// vis = d3.select('#chart').data([memValues]);
+// pie = d3.layout.pie().value(function(d){return d.value;});
+// arcs = vis.selectAll("g.slice").data(pie).select('svg:g')
+// arcs.select('svg:path').attr("fill", function(d, i){
+//         return color(i);
+//     })
+//     .attr("d", function (d) {
+//         // log the result of the arc generator to show how cool it is :)
+//         console.log(arc(d));
+//         return arc(d);
+//     });
 
-  // vis.selectAll("g.slice").data(pie).transition().delay(500).duration(500).append("svg:g").attr("class", "slice")
-  // .append("svg:path")
-  //     .attr("fill", function(d, i){
-  //         return color(i);
-  //     })
-  //     .attr("d", function (d) {
-  //         // log the result of the arc generator to show how cool it is :)
-  //         console.log(arc(d));
-  //         return arc(d);
-  //     });
-},1000)
+//   // vis.selectAll("g.slice").data(pie).transition().delay(500).duration(500).append("svg:g").attr("class", "slice")
+//   // .append("svg:path")
+//   //     .attr("fill", function(d, i){
+//   //         return color(i);
+//   //     })
+//   //     .attr("d", function (d) {
+//   //         // log the result of the arc generator to show how cool it is :)
+//   //         console.log(arc(d));
+//   //         return arc(d);
+//   //     });
+// },1000)
 
 
