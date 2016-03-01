e6d5a61fcaf42aad0676164c72da77d0f4a74b9d
coproductions <constantin.dumba@gmail.com>
2016-03-01 02:31:59 -0800

    copied in pie chart from example

--- public/d3logic.js
+++ public/d3logic.js
@@ -11,15 +11,50 @@ setInterval(function(){
 },1000);
 
 console.log('data',memdata,memdata.Active.size,d3);
-var divs = d3.select('#enter .right')
-.selectAll('div.item')
-.data([memdata.Active.size]);
-divs.enter()
-.append('div').classed('item',true);
-divs.style({
-	width:'20px',
-	height:'40px',
-	margin: '10px',
-	float:'left',
-	'background-color': '#25b0b0'
-})
+// var divs = d3.select('#enter .right')
+// .selectAll('div.item')
+// .data([memdata.Active.size]);
+// divs.enter()
+// .append('div').classed('item',true);
+// divs.style({
+// 	width:'20px',
+// 	height:'40px',
+// 	margin: '10px',
+// 	float:'left',
+// 	'background-color': '#25b0b0'
+// })
+var w = 400;
+var h = 400;
+var r = h/2;
+var color = d3.scale.category20c();
+
+var data = [{"label":"Category A", "value":20},
+              {"label":"Category B", "value":50},
+              {"label":"Category C", "value":30}];
+
+
+var vis = d3.select('#chart').append("svg:svg").data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+var pie = d3.layout.pie().value(function(d){return d.value;});
+
+// declare an arc generator function
+var arc = d3.svg.arc().outerRadius(r);
+
+// select paths, use arc generator to draw
+var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+arcs.append("svg:path")
+    .attr("fill", function(d, i){
+        return color(i);
+    })
+    .attr("d", function (d) {
+        // log the result of the arc generator to show how cool it is :)
+        console.log(arc(d));
+        return arc(d);
+    });
+
+// add the text
+arcs.append("svg:text").attr("transform", function(d){
+      d.innerRadius = 0;
+      d.outerRadius = r;
+    return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+    return data[i].label;}
+    );
