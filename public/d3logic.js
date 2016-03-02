72fcfd7432bbb091c8a2d7ad1cf378898db31b0c
coproductions <constantin.dumba@gmail.com>
2016-03-01 16:27:50 -0800

    added chart.jd

--- public/d3logic.js
+++ public/d3logic.js
@@ -73,71 +73,42 @@ setInterval(function(){
   .text(function(d,i){return d.label+' '+d.value.toFixed(2)+'%';})
 },1000)
 
+// chart.js implementation
 
+var ctx = $("#myChart").get(0).getContext("2d");
 
+Chart.defaults.global = {
+    // Boolean - Whether to animate the chart
+    animation: true,
+        // Function - Will fire on animation progression.
+    onAnimationProgress: function(){
+      console.log('animation progress')
+    },
 
+    // Function - Will fire on animation completion.
+    onAnimationComplete: function(){
+      console.log('animation complete')
+    }
+  };
 
-
-
-
-// var w = 400;
-// var h = 400;
-// var r = h/2;
-// var color = d3.scale.category20c();
-
-
-
-// var vis = d3.select('#chart').append("svg:svg").data([memValues]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
-// var pie = d3.layout.pie().value(function(d){return d.value;});
-
-// // declare an arc generator function
-// var arc = d3.svg.arc().outerRadius(r);
-
-// // select paths, use arc generator to draw
-// var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
-
-
-// arcs.append("svg:path")
-//     .attr("fill", function(d, i){
-//         return color(i);
-//     })
-//     .attr("d", function (d) {
-//         // log the result of the arc generator to show how cool it is :)
-//         console.log(arc(d));
-//         return arc(d);
-//     });
-
-// // add the text
-// arcs.append("svg:text").attr("transform", function(d){
-//       d.innerRadius = 0;
-//       d.outerRadius = r;
-//     return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
-//     return memValues[i].label;}
-//     );
-
-// setInterval(function(){
-// vis = d3.select('#chart').data([memValues]);
-// pie = d3.layout.pie().value(function(d){return d.value;});
-// arcs = vis.selectAll("g.slice").data(pie).select('svg:g')
-// arcs.select('svg:path').attr("fill", function(d, i){
-//         return color(i);
-//     })
-//     .attr("d", function (d) {
-//         // log the result of the arc generator to show how cool it is :)
-//         console.log(arc(d));
-//         return arc(d);
-//     });
-
-//   // vis.selectAll("g.slice").data(pie).transition().delay(500).duration(500).append("svg:g").attr("class", "slice")
-//   // .append("svg:path")
-//   //     .attr("fill", function(d, i){
-//   //         return color(i);
-//   //     })
-//   //     .attr("d", function (d) {
-//   //         // log the result of the arc generator to show how cool it is :)
-//   //         console.log(arc(d));
-//   //         return arc(d);
-//   //     });
-// },1000)
-
-
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+];
+var myPieChart = new Chart(ctx[0]).Pie(data,options);
