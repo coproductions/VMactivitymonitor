4a80c89a2c3ad7a07a0f01c41f69b9ad1a7d7f5f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-25 13:13:44 +0800

    Delete auxinfo.lua

--- auxinfo.lua
+++
@@ -1,8 +0,0 @@
-package.path = "../?.lua;"..package.path;
-
-local procfs = require("lj2procfs.procfs")
-local putil = require("lj2procfs.print-util")
-
-print("return {")
-putil.printValue(procfs.cpuinfo, "    ", "cpuinfo")
-print("}")
